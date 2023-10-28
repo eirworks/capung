d91ea5deb3890cca107de73d63a97ef04a821bae
Rully <hvsciverius@gmail.com>
2023-10-28 10:01:40 +0700

    add user login

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::truncate();
+
+        User::factory()->create([
+            'email' => 'dev@cc.cc',
+            'password' => \Hash::make('dev'),
+        ]);
+
+        User::factory()->count(10)->create();
+    }
+}
